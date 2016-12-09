09d078900344d4ed7d710a5ccefee356c3f34d80
Tony <hildebrandanthony@gmail.com>
2016-12-09 00:21:05 -0800

    Used TakePicture For GUI

--- WTTBMMIGUI.m
+++ WTTBMMIGUI.m
@@ -79,35 +79,12 @@ function pbPreview_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Add NET assembly
-% May need to change specific location of library
-NET.addAssembly('C:\Program Files\Thorlabs\Scientific Imaging\DCx Camera Support\Develop\DotNet\uc480DotNet.dll');
-% Create camera object handle
-cam = uc480.Camera;
-% Open the 1st available camera
-cam.Init(0);
-% Set display mode to bitmap (DiB)
-cam.Display.Mode.Set(uc480.Defines.DisplayMode.DiB);
-% Set color mode to 8-bit RGB
-cam.PixelFormat.Set(uc480.Defines.ColorMode.RGBA8Packed);
-% Set trigger mode to software (single image acquisition)
-cam.Trigger.Set(uc480.Defines.TriggerMode.Software);
-% Allocate image memory
-[~, MemId] = cam.Memory.Allocate(true);
-% Obtain image information
-[~, Width, Height, Bits, ~] = cam.Memory.Inquire(MemId);
-% Acquire image
-cam.Acquisition.Freeze(uc480.Defines.DeviceParameter.Wait);
-% Copy image from memory
-[~, tmp] = cam.Memory.CopyToArray(MemId);
-% Reshape image
-Data = reshape(uint8(tmp), [Bits/8, Width, Height]);
-Data = Data(1:3, 1:Width, 1:Height);
-Data = permute(Data, [3,2,1]);
+%Take a picture and show it in axes 1
+Data = TakePicture;
 % Display Image
-himg = imshow(Data);
-% Close camera
-cam.Exit;
+imshow(Data);
+
+
 
 % --- Executes on button press in pbSaturation.
 function pbSaturation_Callback(hObject, eventdata, handles)
@@ -115,17 +92,17 @@ function pbSaturation_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-TakePicture
-% Display Image
-himg = imshow(Data);
-% Close camera
-cam.Exit;
-histogram(Data(:,:,:));
+%Take a picture and add it for analysis
+Data = TakePicture;
+
+%make histogram of saturation
+histogram(Data(:,:,2));
 Datamaxcol = max(Data);
 Datamaxrow = max(Datamaxcol);
 Datamaxrowgrn = Datamaxrow(1,2);
 xlabel('Pixel Values');
 title(['          Max Value is ', num2str(Datamaxrowgrn) ,'']);
+
  
 % --- Executes on button press in pbOrangeLinear.
 function pbOrangeLinear_Callback(hObject, eventdata, handles)
@@ -133,38 +110,7 @@ function pbOrangeLinear_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Add NET assembly
-% May need to change specific location of library
-NET.addAssembly('C:\Program Files\Thorlabs\Scientific Imaging\DCx Camera Support\Develop\DotNet\uc480DotNet.dll');
-% Create camera object handle
-%Change for Git
-cam = uc480.Camera;
-% Open the 1st available camera
-cam.Init(0);
-% Set display mode to bitmap (DiB)
-cam.Display.Mode.Set(uc480.Defines.DisplayMode.DiB);
-% Set color mode to 8-bit RGB
-cam.PixelFormat.Set(uc480.Defines.ColorMode.RGBA8Packed);
-% Set trigger mode to software (single image acquisition)
-cam.Trigger.Set(uc480.Defines.TriggerMode.Software);
-%Get 5 Frames to average for the particular setting
-cam.Trigger.Set(uc480.TriggerCounter.Reset(5));
-% Allocate image memory
-[~, MemId] = cam.Memory.Allocate(true);
-% Obtain image information
-[~, Width, Height, Bits, Frame] = cam.Memory.Inquire(MemId);
-% Acquire image
-cam.Acquisition.Freeze(uc480.Defines.DeviceParameter.Wait);
-% Copy image from memory
-[~, tmp] = cam.Memory.CopyToArray(MemId);
-% Reshape image
-Data = reshape(uint8(tmp), [Bits/8, Width, Height]);
-Data = Data(1:3, 1:Width, 1:Height,1:Frame);
-Data = permute(Data, [3,2,1]);
-% Display Image
-himg = imshow(Data);
-% Close camera
-cam.Exit;
+
 
 % --- Executes on button press in pbGreenLinear.
 function pbGreenLinear_Callback(hObject, eventdata, handles)
