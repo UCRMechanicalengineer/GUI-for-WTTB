8b06b53d16ca14e19a97cd93d82b60a16a3702f5
Tony <hildebrandanthony@gmail.com>
2016-12-13 20:27:46 -0800

    Add Function For Laser Polarization

---
+++ StokesVectorOfLaser.m
@@ -0,0 +1,42 @@
+function [LaserPolarization] = StokesVectorOfLaser(...
+    FinalPictureOrangeLinear,FinalPictureGreenLinear,...
+    FinalPictureBlueLinear,FinalPictureBlueCircular)
+
+%These are the top lines for the positions for CP1
+Io = [0.25427 0.0309288 0 -0.236628];
+Ig = [0.25427 0 0.0309288 -0.236628];
+Ib = [0.25427 0 -0.0309288 -0.236628];
+Ibc = [0.25427 0 -0.238641 0];
+%Here is the matrix for the inverse multiplication(The top of every
+%polarization matrix multipied with the stokes vector.)
+matrixmult = [Io;Ig;Ib;Ibc]; 
+
+%OrangeLinear sums 
+SumOLCol = sum(FinalPictureOrangeLinear);
+SumOLRow = sum(SumOLCol);
+OrangeLnrGrnChnlSum = SumOLRow(:,:,2);
+
+%GreenLinear sums
+SumGLCol = sum(FinalPictureGreenLinear);
+SumGLRow = sum(SumGLCol);
+GreenLnrGrnChnlSum = SumGLRow(:,:,2);
+
+%BlueLinear sums
+SumBLCol = sum(FinalPictureBlueLinear);
+SumBLRow = sum(SumBLCol);
+BlueLnrGrnChnlSum = SumBLRow(:,:,2);
+
+%BlueCircular sums
+SumBCCol = sum(FinalPictureBlueCircular);
+SumBCRow = sum(SumBCCol);
+BlueCirGrnChnlSum = SumBCRow(:,:,2);
+
+%Matrix of the total intensitys of the different polarization states
+pixelsum = [OrangeLnrGrnChnlSum;GreenLnrGrnChnlSum;BlueLnrGrnChnlSum;BlueCirGrnChnlSum];
+
+%now multiply the sum of the images by the inverse of the polarization
+%matrix
+S = linsolve(matrixmult,pixelsum);
+
+%normalizing the stokes vector
+handles.LaserPolarization = S/S(1,1);
