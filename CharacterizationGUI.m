aeaee779ae2c30ec99d0213ee1ccb7726d99ca75
Tony <hildebrandanthony@gmail.com>
2017-03-27 22:03:43 -0700

    changes loof to average pictures in black and others

--- CharacterizationGUI.m
+++ CharacterizationGUI.m
@@ -69,7 +69,7 @@ guidata(hObject, handles);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = CharacterizationGUI_OutputFcn(hObject, eventdata, handles) 
+function varargout = CharacterizationGUI_OutputFcn(~, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
